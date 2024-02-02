24ef0f8c86a45bedc5d8e49a45f63ac0f33fcb62
Josh Bostick <jbostick99@gmail.com>
2024-02-02 13:27:59 -0600

    in progress

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using static System.Net.WebRequestMethods;
+
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+
+            RonVSKanyeAPI.Kanye();
+            RonVSKanyeAPI.Ron();
+
         }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,32 @@ using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        public static void Kanye()
+        {
+            var client = new HttpClient();
+
+            string kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye: {kanyeQuote}.");
+        }
+       
+        public static void Ron()
+        {
+            var client = new HttpClient();
+
+            string ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronReponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronReponse).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
+
+            Console.WriteLine($"Ron: {ronQuote}");
+        }
     }
 }
